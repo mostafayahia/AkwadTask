14abaa7462d63005fe780e6197ebaf68b5fcd496
root <root@DESKTOP-20GDPA0.localdomain>
2021-02-28 19:24:45 +0200

    feat: add plain object data models

---
+++ Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AkwadTask.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int JobID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime HiringDate { get; set; }
+
+        public string Address { get; set; }
+
+        public string Mobile { get; set; }
+
+        public int Salary { get; set; }
+
+        public int IdNumber { get; set; }
+    }
+}
