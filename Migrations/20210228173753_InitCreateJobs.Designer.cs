52adfda41ba69d7ec3be2896ee4137ceb69de940
root <root@DESKTOP-20GDPA0.localdomain>
2021-02-28 19:42:07 +0200

    chore: add migration for job table

--- Migrations/20210228173753_InitCreateJobs.Designer.cs
+++ Migrations/20210228173753_InitCreateJobs.Designer.cs
@@ -3,14 +3,16 @@ using AkwadTask.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AkwadTask.Migrations
 {
     [DbContext(typeof(AkwadTaskContext))]
-    partial class AkwadTaskContextModelSnapshot : ModelSnapshot
+    [Migration("20210228173753_InitCreateJobs")]
+    partial class InitCreateJobs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210228173753_InitCreateJobs.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AkwadTask.Migrations
+{
+    public partial class InitCreateJobs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Job",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartmentID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Job", x => x.ID);
+                    table.ForeignKey("FK_Department", x => x.DepartmentID, "Department", "ID");
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Job");
+        }
+    }
+}
